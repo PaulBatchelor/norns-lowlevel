246604035ad4415c13d55b430c210ebbeeec6159
Paul Batchelor <thisispaulbatchelor@gmail.com>
2019-07-20 23:56:19 +0700

    some cleanup

--- fbtest.c
+++ fbtest.c
@@ -1,14 +1,3 @@
-/*
-To test that the Linux framebuffer is set up correctly, and that the device permissions
-are correct, use the program below which opens the frame buffer and draws a gradient-
-filled red square:
-
-retrieved from:
-Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
-
-http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
-*/
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
