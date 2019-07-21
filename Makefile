314e5c2a417bca3b0d075763585c9d05b9a1631e
Paul Batchelor <thisispaulbatchelor@gmail.com>
2019-07-21 21:02:04 +0700

    added knob test

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 CFLAGS=-Wall -pedantic -std=c89 -O2
 
+default: fbtest knob
+
 fbtest: fbtest.c
+	$(CC) $(CFLAGS) $< -o $@
+	    
+knob: knob.c
 	$(CC) $(CFLAGS) $< -o $@
 
 
 clean:
 	$(RM) fbtest
+	$(RM) knob
 
---
+++ knob.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <linux/input.h>
+#include <signal.h>
+
+int running = 1;
+
+void quit(int sig)
+{
+    printf("Closing.\n");
+    running = 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int fid;
+    int rc;
+    struct input_event evt[8];
+    int nevts;
+    int e;
+
+
+    signal(SIGINT, quit);
+
+    fid = open(
+        "/dev/input/by-path/platform-soc:knob1-event",
+        O_RDONLY | O_NONBLOCK);
+
+
+    running = 1;
+
+    while(running) {
+        rc = read(fid, evt, sizeof(struct input_event) * 8);
+        if(rc != -1) {
+            nevts = rc / sizeof(struct input_event);
+            for(e = 0; e < nevts; e++) {
+                if(evt[e].type) {
+                    printf("%d\n", evt[e].value);
+                }
+            }
+        }
+        usleep(10);
+    }
+
+    close(fid);
+    return 0;
+}
