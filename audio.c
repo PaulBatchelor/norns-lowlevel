0f1682c18ee8fd1ab1199e45c66f6ef6b10ae2ae
Paul Batchelor <thisispaulbatchelor@gmail.com>
2019-07-30 05:14:14 +0700

    audio example stops with key input

--- audio.c
+++ audio.c
@@ -116,7 +116,8 @@ int main(int argc, char *argv[])
     out[1] = NULL;
     client = NULL;
     audio_start();
-    sleep(10);
+    printf("Hit any key to stop.\n");
+    fgetc(stdin);
     audio_stop();
     return 0;
 }
